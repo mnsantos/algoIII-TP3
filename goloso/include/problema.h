5d9a6cd10a8fa312727ce453d5f13dae6c97c33d
mushucata <mushucata@gmail.com>
2013-11-16 15:13:46 -0300

    empezando goloso

---
+++ goloso/include/problema.h
@@ -0,0 +1,40 @@
+#ifndef PROBLEMA_H
+#define PROBLEMA_H
+
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <fstream>
+#include <cassert>
+#include <algorithm>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+struct Nodo{
+	vector<int> adyacentes;
+	int id;
+};
+
+struct Grafo{
+	vector<Nodo> nodos;
+	int cantNodos;	
+	
+	void mostrarGrafo(ostream&);	
+};
+
+struct Problema{
+	Problema (istream&);
+	void mostrarResultado(ostream& os);
+	void findCliques(vector<int>,vector<int>,vector<int>);
+	void resolver();
+	vector<int> interseccion(vector<int>&, int);
+	int frontera(vector<int>&);
+	
+	Grafo g;
+	vector<int> cliqueMaxFrontera;
+	int maxFrontera;
+};
+
+#endif
