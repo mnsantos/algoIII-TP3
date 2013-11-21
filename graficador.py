a665e4f1e407374e72ec6aa1030ebda778c10406
Martin Nicolas Santos <mnsantos@ws7.labo5>
2013-11-21 13:44:23 -0300

    cambios en el codigo e informe del exacto y graficador

--- graficador.py
+++ graficador.py
@@ -33,5 +33,26 @@ plt.figure(0)
 plt.title(titulo)
 plt.xlabel("cantNodos")
 plt.ylabel("Miles de ciclos")
-plt.plot(x,y)
+
+fig, ax = plt.subplots()
+ax.plot(x,y, label="Algoritmo")
+z = [ i * i * 2**i for i in x ] #funcion para graficar junto a la del algoritmo
+ax.plot(x,z,label="n^2 * 2^n")
+
+# Now add the legend with some customizations.
+legend = ax.legend(loc='upper center', shadow=True)
+
+# The frame is matplotlib.patches.Rectangle instance surrounding the legend.
+frame = legend.get_frame()
+frame.set_facecolor('0.90')
+
+# Set the fontsize
+for label in legend.get_texts():
+    label.set_fontsize('large')
+
+for label in legend.get_lines():
+    label.set_linewidth(1.5)  # the legend line width
+    
 savefig(destino)
+
+
